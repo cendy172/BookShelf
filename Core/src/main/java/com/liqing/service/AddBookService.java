4444c93d87e67dfcb315b88bf53c24cf5e18c606
liqing <cendy172@gmail.com>
2014-07-03 19:53:34 +0800

    add validator for adding a book.

--- Core/src/main/java/com/liqing/service/AddBookService.java
+++ Core/src/main/java/com/liqing/service/AddBookService.java
@@ -1,18 +1,26 @@
 package com.liqing.service;
 
 import com.liqing.domain.Book;
+import com.liqing.validator.AddBookValidator;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 public class AddBookService {
 
     private JdbcTemplate jdbcTemplate;
+    private AddBookValidator addBookValidator;
 
-    public AddBookService(JdbcTemplate jdbcTemplate) {
+    public AddBookService(JdbcTemplate jdbcTemplate, AddBookValidator addBookValidator) {
         this.jdbcTemplate = jdbcTemplate;
+        this.addBookValidator = addBookValidator;
     }
 
     public void addBook(Book book) {
-        jdbcTemplate.execute("insert into book values(" + book.getIsbn() + ",\"" + book.getName() + "\"," + book.getPrice() + ",\"" + book.getAuthor() + "\")");
+        if(addBookValidator.validateBook(book)){
+            jdbcTemplate.execute("insert into book values(" + book.getIsbn() + ",\"" + book.getName() + "\"," + book.getPrice() + ",\"" + book.getAuthor() + "\")");
+        }else{
+            throw new RuntimeException();
+        }
+
     }
 
 }
---
+++ Core/src/main/java/com/liqing/validator/AddBookValidator.java
@@ -0,0 +1,9 @@
+package com.liqing.validator;
+
+import com.liqing.domain.Book;
+
+public class AddBookValidator {
+    public boolean validateBook(Book book){
+        return true;
+    }
+}
